6d39a51676bc6ee9dab148dddae599e9a995d8e9
Recep Battal <recep.buss@gmail.com>
2023-07-05 03:52:14 +0300

    Update client.c

--- minitalk/client.c
+++ minitalk/client.c
@@ -13,16 +13,6 @@
 #include <unistd.h>
 #include <signal.h>
 
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 int	ft_atoi(const char *str)
 {
 	int	total;
